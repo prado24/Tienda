a85349e3ddb881d381666423dcca75bbc607ec2e
Maria Prado <pradojimenezmaria@gmail.com>
2022-09-17 13:00:03 -0500

    Se agrego Entidades

---
+++ EntidadesT/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesT
+{
+    public class Producto
+    {
+        public Producto(int idproducto, string nombre, string descripcion, double precio)
+        {
+            Idproducto = idproducto;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Precio = precio;
+        }
+
+        public int Idproducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+    }
+}
