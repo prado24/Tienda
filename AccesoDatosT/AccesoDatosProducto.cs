1333ac2c668d1cd57490b78c0056afd5b8951532
Maria Prado <pradojimenezmaria@gmail.com>
2022-09-17 13:10:09 -0500

    Se agrego AccesoDatos

---
+++ AccesoDatosT/AccesoDatosProducto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadesT;
+using ConectarBd;
+using System.Data;
+
+namespace AccesoDatosT
+{
+    public class AccesoDatosProducto:IEntidades
+    {
+        Base b = new Base("Localhost", "root", "", "tienda", 3306);
+
+        public void Borrar(dynamic Entidad)
+        {
+            b.comando(String.Format("call deleteProducto({0})",
+            Entidad.Idproducto));
+        }
+
+        public void Guardar(dynamic Entidad)
+        {
+            b.comando(string.Format("call insertProducto(" +
+              "'{0}','{1}',{2},{3})", Entidad.Nombre,
+              Entidad.Descricpion, Entidad.Precio, Entidad.Idproducto));
+        }
+
+        public DataSet Mostrar(string filtro)
+        {
+            return b.Obtener
+            (string.Format("call showProducto('%{0}%')",
+            filtro), "producto"); ;
+        }
+    }
+}
---
+++ AccesoDatosT/IEntidades.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadesT;
+using ConectarBd;
+using System.Data;
+
+namespace AccesoDatosT
+{
+    public interface IEntidades
+    {
+        void Guardar(dynamic Entidad);
+        void Borrar(dynamic Entidad);
+        DataSet Mostrar(string filtro);
+    }
+}
